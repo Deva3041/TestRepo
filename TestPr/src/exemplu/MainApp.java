c29a103f4f0d98cbad6085f5065d05d7b55e949b
Developer1 Laboratory <developer.a3041@gmail.com>
2023-02-11 08:51:53 +0200

    Un alt commit de test

--- TestPr/src/exemplu/MainApp.java
+++ TestPr/src/exemplu/MainApp.java
@@ -3,9 +3,13 @@ package exemplu;
 public class MainApp {
 
 	public static void main(String[] args) {
-		Persoana p=new Persoana("Test", 1);
-		System.out.println(p);
+		Persoana p1=new Persoana("Test1", 1);
+		System.out.println(p1);
+		
+		Persoana p2=new Persoana("Test2", 2);
+		System.out.println(p1);
 
+		
 	}
 
 }
